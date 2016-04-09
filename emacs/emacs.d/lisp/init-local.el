81e558bcd92e911219b44213df2feb364978eabd
Hen haim Sapir <chen.sapir@me.com>
2016-04-09 16:28:38 -0400

    added windmove shortcut

---
+++ emacs/emacs.d/lisp/init-local.el
@@ -0,0 +1,8 @@
+;; set window switching shortcuts
+
+(global-set-key (kbd "C-x <up>") 'windmove-up)
+(global-set-key (kbd "C-x <down>") 'windmove-down)
+(global-set-key (kbd "C-x <left>") 'windmove-left)
+(global-set-key (kbd "C-x <right>") 'windmove-right)
+
+(provide 'init-local)
